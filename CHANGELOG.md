c723bf102d6e9d7d939a79b292b3b18c2188a635
gmpassos <gracilianomp@gmail.com>
2025-01-03 22:43:57 -0300

    v1.0.1
    - export 'package:js_interop_utils/js_interop_utils.dart'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- export 'package:js_interop_utils/js_interop_utils.dart'
+
 ## 1.0.0
 
 - Initial version.
--- lib/web_utils.dart
+++ lib/web_utils.dart
@@ -3,6 +3,7 @@
 /// - Exports `package:web/web.dart`
 library;
 
+export 'package:js_interop_utils/js_interop_utils.dart';
 export 'package:web/web.dart';
 
 export 'src/web_utils_extensions.dart';
